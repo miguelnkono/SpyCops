849be838644c91b539013bb81e9357ef74605eef
Nkono Ndeme Miguel <nkonondememiguel7@gmail.com>
2025-03-05 22:28:17 +0100

    started working on my java project. Face Recognition

---
+++ src/main/java/com/dream/spycops/controllers/RegisterController.java
@@ -0,0 +1,28 @@
+package com.dream.spycops.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+
+    @FXML
+    public TextField email;
+    @FXML
+    public TextField lastName;
+    @FXML
+    public TextField firstName;
+    @FXML
+    public PasswordField password;
+
+    @FXML
+    public void cancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void validate(ActionEvent event) {
+
+    }
+}
